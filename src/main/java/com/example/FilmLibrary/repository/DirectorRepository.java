274dbea40ee5446119a5f98594a965a6ad1a045e
Maryna <nikishina.maryna@gmail.com>
2022-01-19 17:07:17 +0200

    change Repository

--- src/main/java/com/example/FilmLibrary/repository/DirectorRepository.java
+++ src/main/java/com/example/FilmLibrary/repository/DirectorRepository.java
@@ -4,8 +4,9 @@ import com.example.FilmLibrary.entity.Director;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface DirectorRepository extends JpaRepository<Director,Long> {
     Director findByLastNameDirector(String lastNameDirector);
     Page<Director> findAll(Pageable pageable);
